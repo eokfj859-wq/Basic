7329a1d8c1f7680f24cfa9999afe8e769b54484a
JaydeJ <eokfj85.9@gmail.com>
2026-01-03 14:03:34 +0000

    js기초: 연산자, 조건문 실습

---
+++ Lang/01_JavaScript/Grammar/02_operator_conditional.js
@@ -0,0 +1,16 @@
+// 2강: 연산자와 조건문 실습
+let age = 20;
+if (age >= 19) {
+    console.log("성인입니다.");
+}
+
+//학점계산기
+let score = 85;
+
+if (score >= 90) {
+    console.log("A학점");
+} else if (score >= 80) {
+    console.log("B학점");
+} else {
+    console.log("C학점");
+}
